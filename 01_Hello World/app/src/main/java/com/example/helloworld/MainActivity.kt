ece757705f051371c68d85c81e5e99e854314080
Chayapon Puakalong <dream254111@hotmail.com>
2021-06-10 14:23:19 +0700

    Show dice image depends on random number

--- 01_Hello World/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ 01_Hello World/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.helloworld
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.lang.Exception
@@ -10,6 +13,7 @@ import java.lang.Exception
 class MainActivity : AppCompatActivity() {
     lateinit var btnRoll: Button
     lateinit var btnCountUp: Button
+    lateinit var imgDice: ImageView
     lateinit var txtResult: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         btnRoll = findViewById(R.id.btn_roll)
         btnCountUp = findViewById(R.id.btn_count_up)
+        imgDice = findViewById(R.id.img_dice)
         txtResult = findViewById(R.id.txt_result)
 
         btnRoll.setOnClickListener { rollDice() }
@@ -25,18 +30,38 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
+        imgDice.visibility = View.VISIBLE
         Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
 
         val randomInt = (1..6).random()
         txtResult.text = randomInt.toString()
+        imgDice.setImageResource(when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        })
     }
 
     private fun upTextValue() {
+        imgDice.visibility = View.VISIBLE
         try {
             val value: Int = Integer.parseInt(txtResult.text.toString())
             txtResult.text = if (value != 6) (value + 1).toString() else "6"
+            imgDice.setImageResource(when (value + 1) {
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                6, 7 -> R.drawable.dice_6
+                else -> R.drawable.empty_dice
+            })
         } catch (ex: Exception) {
             txtResult.text = "1"
+            imgDice.setImageResource(R.drawable.dice_1)
         }
     }
 }
