0f8be18064b4c9bab8a57d282e7d1f4bd075e477
Chayapon Puakalong <dream254111@hotmail.com>
2021-06-10 13:31:26 +0700

    Change text to randon number after pressing button

--- 01_Hello World/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ 01_Hello World/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,18 +3,27 @@ package com.example.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnRoll: Button
+    lateinit var txtResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnRoll: Button = findViewById(R.id.btn_roll)
+        btnRoll = findViewById(R.id.btn_roll)
+        txtResult = findViewById(R.id.txt_result)
+
         btnRoll.setOnClickListener { rollDice() }
     }
 
     private fun rollDice() {
         Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+
+        val randomInt = (1..6).random()
+        txtResult.text = randomInt.toString()
     }
 }
