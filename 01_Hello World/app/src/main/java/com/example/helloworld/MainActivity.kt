8fc9cc90862ff982dea3f5aab183b9315af25e2f
Chayapon Puakalong <dream254111@hotmail.com>
2021-06-10 11:51:35 +0700

    Connect button between layout and activity

--- 01_Hello World/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ 01_Hello World/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnRoll: Button = findViewById(R.id.btn_roll)
+        btnRoll.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
     }
 }
