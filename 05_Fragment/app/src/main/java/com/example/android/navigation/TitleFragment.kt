505c80d5d1cc2172bcc57921154acaf5e7ca6d28
Chayapon Puakalong <dream254111@hotmail.com>
2021-06-16 10:57:07 +0700

    Test Comment

--- 05_Fragment/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ 05_Fragment/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -32,6 +32,7 @@ class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+        //test
         return inflater.inflate(R.layout.fragment_title, container, false)
     }
 
