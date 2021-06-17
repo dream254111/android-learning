e5b5608ed6ea9532246ebd9e0f92b332fd4bae15
Chayapon Puakalong <dream254111@hotmail.com>
2021-06-17 18:07:34 +0700

    Share success

--- 05_Fragment/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ 05_Fragment/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -16,28 +16,53 @@
 
 package com.example.android.navigation
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameWonBinding
 
-
 class GameWonFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_game_won, container, false)
-        binding.nextMatchButton.setOnClickListener {
-            it.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
-        }
-        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+    private lateinit var args: GameWonFragmentArgs
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding: FragmentGameWonBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_game_won, container, false)
+        binding.nextMatchButton.setOnClickListener { it.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment()) }
+
+        args = GameWonFragmentArgs.fromBundle(requireArguments())
         Toast.makeText(context, "NumCorrect: ${args.numCorrect}, NumQuestion: ${args.numQuestions}", Toast.LENGTH_LONG).show()
+
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    private fun getShareIntent(): Intent? {
+        return Intent(Intent.ACTION_SEND)
+                .setType("text/plain")
+                .putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+    }
+
+    private fun shareSuccess() {
+        startActivity(getShareIntent()  )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.winner_menu, menu)
+        if (getShareIntent()?.resolveActivity(requireActivity().packageManager) == null) {
+            menu.findItem(R.id.share).isVisible = false
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
