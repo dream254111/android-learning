5e6088cb415f0f9dbfaec13088f55701e4999e13
Chayapon Puakalong <dream254111@hotmail.com>
2021-06-17 09:30:18 +0700

    Add up button

--- 05_Fragment/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ 05_Fragment/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -3,14 +3,26 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        navController = this.findNavController(R.id.navFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 
     // TODO (01) Create the new TitleFragment
