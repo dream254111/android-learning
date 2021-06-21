0158ee5b9f019e43459f006d95ab0662238f56ff
Chayapon Puakalong <dream254111@hotmail.com>
2021-06-21 13:59:03 +0700

    Move data variables and methods to GameViewModel

--- 07_ViewModel/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ 07_ViewModel/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -31,18 +31,7 @@ import com.example.android.guesstheword.databinding.GameFragmentBinding
  * Fragment where the game is played
  */
 class GameFragment : Fragment() {
-
     private lateinit var viewModel: GameViewModel
-
-    // The current word
-    private var word = ""
-
-    // The current score
-    private var score = 0
-
-    // The list of words - the front of the list is the next word to guess
-    private lateinit var wordList: MutableList<String>
-
     private lateinit var binding: GameFragmentBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -56,9 +45,6 @@ class GameFragment : Fragment() {
                 false
         )
 
-        resetList()
-        nextWord()
-
         binding.correctButton.setOnClickListener { onCorrect() }
         binding.skipButton.setOnClickListener { onSkip() }
         updateScoreText()
@@ -68,41 +54,10 @@ class GameFragment : Fragment() {
         Log.i("GameFragment", "Get GameViewModel")
 
         return binding.root
-
     }
 
-    /**
-     * Resets the list of words and randomizes the order
-     */
-    private fun resetList() {
-        wordList = mutableListOf(
-                "queen",
-                "hospital",
-                "basketball",
-                "cat",
-                "change",
-                "snail",
-                "soup",
-                "calendar",
-                "sad",
-                "desk",
-                "guitar",
-                "home",
-                "railway",
-                "zebra",
-                "jelly",
-                "car",
-                "crow",
-                "trade",
-                "bag",
-                "roll",
-                "bubble"
-        )
-        wordList.shuffle()
-    }
 
     /** Methods for buttons presses **/
-
     private fun onSkip() {
         score--
         nextWord()
@@ -113,21 +68,7 @@ class GameFragment : Fragment() {
         nextWord()
     }
 
-    /**
-     * Moves to the next word in the list
-     */
-    private fun nextWord() {
-        if (!wordList.isEmpty()) {
-            //Select and remove a word from the list
-            word = wordList.removeAt(0)
-        }
-        updateWordText()
-        updateScoreText()
-    }
-
-
     /** Methods for updating the UI **/
-
     private fun updateWordText() {
         binding.wordText.text = word
     }
