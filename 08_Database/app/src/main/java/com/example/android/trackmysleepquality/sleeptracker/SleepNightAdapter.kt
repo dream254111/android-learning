a0fff84167d5c9ac57b72d3c54729fa647f26437
Chayapon Puakalong <dream254111@hotmail.com>
2021-06-28 09:31:35 +0700

    Use BindingAdapter

--- 08_Database/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ 08_Database/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -23,18 +23,8 @@ class SleepNightAdapter: ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(S
 
     class ViewHolder private constructor(val binding: SleepNightItemViewBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(item: SleepNight) {
-            val res = itemView.context.resources
-            binding.sleepLength.text = convertDurationToFormatted(item.startTime, item.endTime, res)
-            binding.ratingString.text = convertNumericQualityToString(item.rating, res)
-            binding.ratingImage.setImageResource(when (item.rating) {
-                0 -> R.drawable.ic_sleep_0
-                1 -> R.drawable.ic_sleep_1
-                2 -> R.drawable.ic_sleep_2
-                3 -> R.drawable.ic_sleep_3
-                4 -> R.drawable.ic_sleep_4
-                5 -> R.drawable.ic_sleep_5
-                else -> R.drawable.ic_sleep_active
-            })
+            binding.sleep = item
+            binding.executePendingBindings()
         }
 
         companion object {
