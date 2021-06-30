f13b9609fdbaabfcde35a3ffbed771b6d590d201
Chayapon Puakalong <dream254111@hotmail.com>
2021-06-30 16:54:04 +0700

    Prepare code for the next step

--- 08_Database/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ 08_Database/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -20,11 +20,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridLayout
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.database.SleepNight
@@ -75,6 +77,9 @@ class SleepTrackerFragment : Fragment() {
         viewModel.nights.observe(viewLifecycleOwner, Observer {
             it?.let { adapter.submitList(it) }
         })
+
+        val layoutManager = GridLayoutManager(activity, 5, GridLayoutManager.HORIZONTAL, false)
+        binding.sleepList.layoutManager = layoutManager
 
         binding.sleepTrackerViewModel = viewModel
         binding.setLifecycleOwner(this)
